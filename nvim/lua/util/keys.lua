ff6a2c421bb94bf41ed43e776f950d87a6631b87
Maximov Matvey <maximov-matvi@yandex.ru>
2024-08-17 09:31:57 +0400

    add key bindings

--- nvim/lua/util/keys.lua
+++ nvim/lua/util/keys.lua
@@ -1,3 +1,62 @@
 vim.g.localmapleader = ","
 
-local wk = require("which-key")
+require("which-key")
+
+-- Keybinds
+local keybinds = {
+	-- General
+	{ "<C-s>", action = ":wa<CR>", desc = "Save all files", modes = { "n" } },
+	{
+		"<leader>hh",
+		action = ":nohlsearch<CR>",
+		desc = "Disable search highlight",
+		modes = { "n", "v" },
+	},
+	{
+		"<",
+		action = "<gv",
+		desc = "Unindent selection",
+		modes = { "v" },
+	},
+	{
+		">",
+		action = ">gv",
+		desc = "Indent selection",
+		modes = { "v" },
+	},
+	{
+		"<leader>ss",
+		action = ":sort<CR>",
+		desc = "Sort selection",
+		modes = { "v" },
+	},
+	{
+		"<leader>fc",
+		action = ":foldclose<CR>",
+		desc = "Close fold",
+		modes = { "n", "v" },
+	},
+	{
+		"<leader>fo",
+		action = ":foldopen<CR>",
+		desc = "Open fold",
+		modes = { "n", "v" },
+	},
+	{
+		"<leader>sh",
+		action = ":split<CR>",
+		desc = "New horizontal split",
+		modes = { "n", "v" },
+	},
+	{
+		"<leader>sv",
+		action = ":vsplit<CR>",
+		desc = "New vertical split",
+		modes = { "n", "v" },
+	},
+}
+
+-- Set keybinds
+for _, k in ipairs(keybinds) do
+	vim.keymap.set(k.modes, k[1], k.action, { desc = k.desc })
+end
