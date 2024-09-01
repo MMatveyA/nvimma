26e7fdce6e4ca2134ed52485fb18abd1e0f8f030
Maximov Matvey <maximov-matvi@yandex.ru>
2024-09-01 20:55:46 +0400

    enable all language

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -1,12 +1,3 @@
---  _   _ ____        _   _       _
--- | \ | |  _ \      | \ | |_   _(_)_ __ ___
--- |  \| | | | |_____|  \| \ \ / / | '_ ` _ \
--- | |\  | |_| |_____| |\  |\ V /| | | | | | |
--- |_| \_|____/      |_| \_| \_/ |_|_| |_| |_|
--- Copyright: 2024 NewDawn0
---
--- Author: NewDawn0 (https://github.com/NewDawn0)
-
 -- Utility
 local lsps = require("util.lsps")
 local set = vim.keymap.set
