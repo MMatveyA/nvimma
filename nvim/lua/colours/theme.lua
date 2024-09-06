5459ba6debdf7b4f3984d85297641788c342514b
Maximov Matvey <maximov-matvi@yandex.ru>
2024-09-06 22:58:04 +0400

    format file

--- nvim/lua/colours/theme.lua
+++ nvim/lua/colours/theme.lua
@@ -3,30 +3,30 @@ local M = {}
 
 -- Themes
 M.colours = {
-  ndnvim = {
-    black = "#141b1e",
-    gray4 = "#1c2326",
-    gray3 = "#232a2d",
-    gray2 = "#2e3538",
-    gray = "#3a4144",
-    white3 = "#c8c8c8",
-    white2 = "#d0d0d0",
-    white = "#dadada",
-    orange = "#ff966c",
-    yellow = "#e4c76b",
-    green = "#a3c87d",
-    cyan = "#0db9d7",
-    blue = "#62baff",
-    magenta = "#c099ff",
-    pink = "#fca7ea",
-    red = "#ff757f",
-    teal = "#4fd6be",
-    gold = "#ffc777",
-  },
+	nvimma = {
+		black = "#141b1e",
+		gray4 = "#1c2326",
+		gray3 = "#232a2d",
+		gray2 = "#2e3538",
+		gray = "#3a4144",
+		white3 = "#c8c8c8",
+		white2 = "#d0d0d0",
+		white = "#dadada",
+		orange = "#ff966c",
+		yellow = "#e4c76b",
+		green = "#a3c87d",
+		cyan = "#0db9d7",
+		blue = "#62baff",
+		magenta = "#c099ff",
+		pink = "#fca7ea",
+		red = "#ff757f",
+		teal = "#4fd6be",
+		gold = "#ffc777",
+	},
 }
 
 -- Default theme
-M.default = M.colours.ndnvim
+M.default = M.colours.nvimma
 
 -- Return module
 return M
