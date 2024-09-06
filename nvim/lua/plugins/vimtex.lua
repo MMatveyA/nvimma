6ba4c8355b5669e0f84049b4909981fd9c80025d
Maximov Matvey <maximov-matvi@yandex.ru>
2024-09-06 22:51:47 +0400

    setup zathura as viewer

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,8 +1,9 @@
-vim.g.vimtex_view_general_viewer = "okular"
-vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
-vim.g.tex_flavor = "latex"
+vim.g.vimtex_view_general_viewer = "zathura"
+vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:'@tex' '@pdf'"
 vim.g.vimtex_compiler_progname = "nvr"
+vim.g.tex_flavor = "latex"
 vim.g.vimtex_quickfix_enabled = 1
 vim.g.vimtex_quickfix_method = "pplatex"
+vim.g.vimtex_quickfix_mode = 0
 vim.opt.conceallevel = 2
 vim.g.tex_conceal = "abdmg"
