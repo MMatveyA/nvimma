874a16be99f0f5d64390377636ba057bd6e5f414
Матвей Максимов <matvey@MacBook-Pro-Matvej.local>
2024-09-27 13:46:18 +0400

    patch for macos

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,5 +1,7 @@
-vim.g.vimtex_view_method = "zathura"
-vim.g.vimtex_compiler_progname = "nvr"
+vim.g.vimtex_view_method = "skim"
+vim.g.vimtex_view_skim_sync = 1
+vim.g.vimtex_view_skim_activate = 1
+-- vim.g.vimtex_compiler_progname = "nvr"
 vim.g.tex_flavor = "latex"
 vim.g.vimtex_quickfix_enabled = 1
 vim.g.vimtex_quickfix_method = "pplatex"
