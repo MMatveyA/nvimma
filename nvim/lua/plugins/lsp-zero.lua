1ac990c5bb52a53db7fd8bdd9be5f3a9e6d2e259
Maximov Matvey <maximov-matvi@yandex.ru>
2024-11-14 22:08:47 +0400

    add texlab support

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -122,4 +122,35 @@ lsps.setup_handlers({
 			end,
 		})
 	end,
+	["texlab"] = function()
+		lsps.lspconfig.texlab.setup({
+			cmd = { "texlab" },
+			filetypes = { "tex", "plaintex", "bib" },
+			settings = {
+				texlab = {
+					bibtexFormatter = "texlab",
+					build = {
+						args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+						executable = "latexmk",
+						forwardSearchAfter = false,
+						onSave = false,
+					},
+					chktex = {
+						onEdit = false,
+						onOpenAndSave = false,
+					},
+					diagnosticsDelay = 300,
+					formatterLineLength = 80,
+					forwardSearch = {
+						args = {},
+					},
+					latexFormatter = "latexindent",
+					latexindent = {
+						modifyLineBreaks = false,
+					},
+				},
+			},
+			single_file_support = true,
+		})
+	end,
 })
